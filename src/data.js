2430656c47b3bb78be8a02f05618b59f1d3a9342
Romy Haacke <romy@haacke.tv>
2020-09-18 23:10:16 +0200

    Add button hover styles

--- src/data.js
+++ src/data.js
@@ -4,6 +4,9 @@ const questions = [{
 }, {
     text: 'Was war Tomis erste Sportart?',
     answers: [{text: 'Fußball', right: true}, {text: 'Volleyball', right: false}, {text: 'Leichtathletik', right: false}, {text: 'Puzzlen', right: false}]
+}, {
+    text: 'Wo ist Tomi aufgewachsen?',
+    answers: [{text: 'Warschau', right: false}, {text: 'Im Busch', right: false}, {text: 'Berlin', right: true}, {text: 'Sporthalle', right: false}]
 }]
 
 export default questions;
--- src/quiz.js
+++ src/quiz.js
@@ -8,64 +8,78 @@ import { Route, Router } from 'react-router-dom';
 import AddQuestion from './addQuestion';
 
 const StyledButton = withStyles({
-  root: {
-    background: 'cadetblue',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgb(37 216 214 / 30%)',
-  },
-  label: {
-    textTransform: 'capitalize',
-  },
+    root: {
+        background: 'cadetblue',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgb(37 216 214 / 30%)',
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
 })(Button);
 
 function Quiz() {
-  const [markAnswers, setMarkAnswers] = useState(false);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [selectedQuestion, setSelectedQuestion] = useState(0);
-  
-  data[0].active = true;
+    const [markAnswers, setMarkAnswers] = useState(false);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [selectedQuestion, setSelectedQuestion] = useState(0);
 
-  const checkAnswer = answer => {
-    setSelectedAnswer(answer);
-    setMarkAnswers(true);
-  }
+    data[0].active = true;
 
-  const getAnswerStyle = answer => {
-    if (markAnswers && answer.right) {
-      return 'right-answer';
+    const checkAnswer = answer => {
+        setSelectedAnswer(answer);
+        setMarkAnswers(true);
     }
-    if (markAnswers && answer === selectedAnswer && !answer.right) {
-      return 'wrong-answer';
+
+    const getAnswerStyle = answer => {
+        if (markAnswers && answer.right) {
+            return 'right-answer';
+        }
+        if (markAnswers && answer === selectedAnswer && !answer.right) {
+            return 'wrong-answer';
+        }
     }
-  }
 
-  const nextAnswer = () => {
-    setMarkAnswers(false);
-    setSelectedQuestion(selectedQuestion + 1);
-    console.log(data.length, selectedQuestion)
-  }
+    const nextAnswer = () => {
+        setMarkAnswers(false);
+        setSelectedQuestion(selectedQuestion + 1);
+        console.log(data.length, selectedQuestion)
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {data.map((entry, i) => <div key={i} hidden={i !== selectedQuestion}><Grid spacing={3} container>
-          <Grid item lg={12} md={12}>
-            {entry.text}
-          </Grid>
-          {entry.answers.map((answer, i) => <Grid  className="answer" key={i} item lg={6} md={6}><Button className="answer" onClick={() => checkAnswer(answer)} className={getAnswerStyle(answer)} variant="contained">{answer.text}</Button></Grid>)}
-        </Grid>
-        </div>)}
-        <div hidden={selectedQuestion >= data.length - 1} >
-        <StyledButton variant="contained" onClick={nextAnswer}>WEITER</StyledButton>
+    const lastAnswer = () => {
+        setMarkAnswers(false);
+        setSelectedQuestion(selectedQuestion - 1);
+        console.log(data.length, selectedQuestion)
+    }
 
+    return (
+        <div className="App">
+            <header className="App-header">
+                {data.map((entry, i) => <div key={i} hidden={i !== selectedQuestion}><Grid spacing={3} container>
+                    <Grid item lg={12} md={12}>
+                        {entry.text}
+                    </Grid>
+                    {entry.answers.map((answer, i) => <Grid className="answer" key={i} item lg={6} md={6}><Button className="answer" onClick={() => checkAnswer(answer)} className={getAnswerStyle(answer)} variant="contained">{answer.text}</Button></Grid>)}
+                </Grid>
+                </div>)}
+                <Grid container spacing={3}>
+                    <Grid hidden={selectedQuestion === 0} item lg={12} md={12}>
+                        <div>
+                            <StyledButton variant="contained" onClick={lastAnswer}>ZURÜCK</StyledButton>
+                        </div>
+                    </Grid>
+                    <Grid hidden={selectedQuestion >= data.length - 1} item lg={12} md={12}>
+                        <div>
+                            <StyledButton variant="contained" onClick={nextAnswer}>WEITER</StyledButton>
+                        </div>
+                    </Grid>
+                </Grid>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default Quiz;
